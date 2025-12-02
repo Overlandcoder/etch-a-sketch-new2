0bcb867f8b2d6aff3c138129e16892efcafa8523
Overlander <anuragnandal@hotmail.com>
2025-12-02 18:20:32 -0500

    Create 16x16 grid via functions

---
+++ script.js
@@ -0,0 +1,19 @@
+const grid = document.querySelector(".grid");
+
+function createRows(numOfRows) {
+  for (let i = 0; i < numOfRows; i++) {
+    const row = document.createElement("div");
+    createCubes(16, row);
+    grid.appendChild(row);
+  }
+}
+
+function createCubes(numOfCubes, parent) {
+  for (let i = 0; i < numOfCubes; i++) {
+    cube = document.createElement("div");
+    cube.classList.add("cube")
+    parent.appendChild(cube);
+  }
+}
+
+createRows(16);
